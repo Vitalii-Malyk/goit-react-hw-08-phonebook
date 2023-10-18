5a704f2845dfeaee912df4ee7fe333a41a1a6cbe
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2023-10-18 20:41:53 +0000

    Deploying to gh-pages from @ Vitalii-Malyk/goit-react-hw-08-phonebook@47379a01b434fbfb1b598b3b465434a6fcad4f98 🚀

---
+++ static/js/35.8d3495c6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[35],{35:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Registration"})}}}]);
+//# sourceMappingURL=35.8d3495c6.chunk.js.map
---
+++ static/js/982.f91f917b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[982],{982:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)("div",{children:"Login"})}}}]);
+//# sourceMappingURL=982.f91f917b.chunk.js.map
